fc3a18e45220eebaa242835ac8e4b97195cf428c
Piers Kenyon <piers.kenyon@cgi.com>
2025-10-09 21:16:34 +0100

    chore: end of day wip

--- appcli.go
+++ appcli.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"time"
 )
@@ -14,17 +15,18 @@ import (
 const (
 	dataFileName       string      = "todolist.json"
 	openmode           int         = os.O_RDWR | os.O_CREATE
+	opentruncatemode   int         = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
 	readwrite          os.FileMode = 0600
 	friendlyDateFormat string      = time.RFC3339
 )
 
 // holds our item
 type TodoListItem struct {
-	Id          string `json:"id"`
+	Line        int    `json:"line"`
 	Description string `json:"description"`
 	State       int    `json:"state"`
 	Created     string `json:"created"` // friendly date
-	Line        int    `json:"line"`
+	Id          string `json:"id"`
 }
 
 // "not started", "started", or "completed", "other etc"
@@ -48,7 +50,7 @@ func main() {
 	var flagAdd = flag.String("add", "", "add to todolist item")
 	var flagUpdate = flag.Int("update", -1, "update to todolist item")
 	var flagDelete = flag.Int("delete", -1, "delete item in todolist item")
-	var flagList = flag.String("list", "", "List items in to todolist item")
+	var flagList = flag.Int("list", -1, "List items in to todolist item (0 for all)")
 	// var flagTask = flag.String("name", "", "task name")
 
 	// scratch debug info
@@ -58,10 +60,37 @@ func main() {
 	fmt.Printf("lastArgument is: %s\n", lastArgument(os.Args))
 	fmt.Printf("appname is %s\n", appName())
 
+	// for loop understanding
+	fmt.Println("")
+	if len(os.Args) > 1 {
+		for index := range len(os.Args[1:]) {
+			fmt.Printf("arguments item: %s\n", os.Args[index+1])
+		}
+	}
+
 	// our global list
 	todoList := []TodoListItem{}
 
-	// grab the flag input state
+	// file resolver stuff for filer.go ?
+	storageDir, storageErr := storagePath()
+	if storageErr == nil {
+		fmt.Printf("Storage path:  %s\n", storageDir)
+	} else {
+		fmt.Printf("Storage path check failed:  %s\n", storageErr)
+	}
+
+	storageFile := fmt.Sprintf("%s\\%s", storageDir, dataFileName)
+	fmt.Printf("Will be saving data to:  %s\n", storageFile)
+	// end file resolver
+
+	// pickup current list before process command
+	todoList = restore(storageFile)
+
+	fmt.Printf("BeforeActions: Items in todoList: %d\n", len(todoList))
+	fmt.Println(todoList)
+	fmt.Println(strings.Repeat("*", 25))
+
+	// grab the flag input state from command line
 	flag.Parse()
 
 	fmt.Printf("Status is: %d\n", *flagTaskStatus)
@@ -78,81 +107,27 @@ func main() {
 	if *flagAdd != "" {
 		fmt.Printf("%s\n", "wanting to add a todo item")
 		fmt.Printf("[%s]\n", *flagAdd)
-
-		// try a for to see effect of many
-		for i := range 5 {
-			item := newTodoListItem(*flagAdd, StateNotStarted, len(todoList)+1)
-			todoList = append(todoList, *item)
-			i++
-		}
+		item := newTodoListItem(*flagAdd, StateNotStarted, len(todoList)+1)
+		todoList = append(todoList, *item)
 	}
+
 	if *flagUpdate != -1 {
 		// todo
 	}
 	if *flagDelete != -1 {
-		// todo
+		fmt.Printf("FlagDelete [%d]\n", *flagDelete)
+		todoList = deleteTask(todoList, *flagDelete)
+		listTask(todoList, -1)
 	}
-	if *flagList != "" {
-		//todo
+	if *flagList > -1 {
+		fmt.Printf("FlagList [%d]\n", *flagList)
+		listTask(todoList, *flagList)
 	}
-
-	// for loop understanding
-	fmt.Println("")
-	if len(os.Args) > 1 {
-		for index := range len(os.Args[1:]) {
-			fmt.Printf("arguments item: %s\n", os.Args[index+1])
-		}
-	}
-
-	// for loop understanding
-	// fmt.Println("line report etc")
-	// for index := range len(todolist) {
-	// 	fmt.Printf("arguments item: %s\n", os.Args[index+1])
-	// }
-
-	// file stuff filer.go ?
-	storageDir, storageErr := storagePath()
-	if storageErr == nil {
-		fmt.Printf("Storage path:  %s\n", storageDir)
-	} else {
-		fmt.Printf("Storage path check failed:  %s\n", storageErr)
-	}
-
-	storageFile := fmt.Sprintf("%s\\%s", storageDir, dataFileName)
-	fmt.Printf("Will saving data to:  %s\n", storageFile)
-
-	destination, err := createDataFile(storageFile)
-	if err == nil {
-		fmt.Printf("Should have created:  %s\n", storageFile)
-	}
-
-	// debug info
-	fmt.Printf("os file pointer is %p\n", destination)
-
-	// debug data in the todo list
-	s := stringifyList(todoList)
-	fmt.Printf("Data list as json:\n%s\n", s)
 
 	// write to the file
-	// saveData := []byte(stringifyList(todoList))
-	// save(destination, saveData)
-
-	// try a restore
-	// restored := restore(destination)
-	// fmt.Printf("restored string \n%s\n", string(restored))
-	// data := []byte(string(restored))
-	// restoredList := []TodoListItem{}
-	// restoredError := json.Unmarshal(data, &restoredList)
-	// fmt.Println(restoredError)
-	// fmt.Println(restoredList)
-
-	checkRestore(destination)
+	saveList := []byte(stringifyList(todoList))
+	save(storageFile, saveList)
 }
-
-// // check the read
-// var restored, _ = io.ReadAll(destination)
-// fmt.Printf("restored string \n%s\n", string(restored))
-// //
 
 func appName() string {
 	return strings.Split(filepath.Base(os.Args[0]), ".")[0]
@@ -177,8 +152,8 @@ func createAppPath(fileFolder string) string {
 	return fileFolder
 }
 
-func createDataFile(fileName string) (*os.File, error) {
-	fi, err := os.OpenFile(fileName, openmode, readwrite)
+func createDataFile(fileName string, mode int) (*os.File, error) {
+	fi, err := os.OpenFile(fileName, mode, readwrite)
 	return fi, err
 }
 
@@ -207,25 +182,52 @@ func newTodoListItem(description string, state int, line int) *TodoListItem {
 	return &item
 }
 
-func save(w io.Writer, data []byte) int {
+func save(storageFile string, data []byte) {
+	destination, err := createDataFile(storageFile, opentruncatemode)
+	if destination != nil {
+		defer destination.Close()
+	}
+	if err == nil {
+		saveData(destination, data)
+	}
+}
+
+func saveData(w io.Writer, data []byte) int {
 	bytes, _ := w.Write(data)
 	return bytes
 }
 
-func restore(r io.Reader) []byte {
-	var restored, _ = io.ReadAll(r)
-	return restored
+func restore(storageFile string) []TodoListItem {
+	destination, err := createDataFile(storageFile, openmode)
+	if destination != nil {
+		defer destination.Close()
+	}
+	if err != nil {
+		return []TodoListItem{}
+	}
+	return restoreList(destination)
 }
 
-func checkRestore(destination io.Reader) {
-	// try a restore and look at contents
-	restored := restore(destination)
-	fmt.Printf("restored string \n%s\n", string(restored))
+func restoreList(destination io.Reader) []TodoListItem {
+	restored := restoreData(destination)
+	if len(restored) == 0 {
+		fmt.Printf("returning empty list restored empty")
+		return []TodoListItem{}
+	}
 	data := []byte(string(restored))
 	restoredList := []TodoListItem{}
-	restoredError := json.Unmarshal(data, &restoredList)
-	fmt.Println(restoredError)
-	fmt.Println(restoredList)
+	jsonError := json.Unmarshal(data, &restoredList)
+	if jsonError != nil {
+		fmt.Println(jsonError)
+		fmt.Printf("returning empty list json error")
+		return []TodoListItem{}
+	}
+	return restoredList
+}
+
+func restoreData(r io.Reader) []byte {
+	var restored, _ = io.ReadAll(r)
+	return restored
 }
 
 // could use other choices of id layout/format here. guid etc for safer uniqueness?
@@ -237,4 +239,27 @@ func generateId() string {
 func generatedFriendlyDate() string {
 	t := time.Now().UTC()
 	return fmt.Sprintf("%s", t.Format(friendlyDateFormat))
+}
+
+func listTask(l []TodoListItem, index int) {
+	fmt.Printf("%s Todo List items %s\n", strings.Repeat("*", 3), strings.Repeat("*", 3))
+	if len(l) > 0 {
+		if index > 0 && index <= len(l)+1 {
+			fmt.Printf("%+v\n", l[index-1])
+		} else {
+			for i := range len(l) {
+				fmt.Printf("%+v\n", l[i])
+			}
+		}
+	}
+}
+
+func deleteTask(l []TodoListItem, index int) []TodoListItem {
+	if len(l) > 0 {
+		if index > 0 && index <= len(l) {
+			fmt.Printf("%s Deleting item %d %s\n", strings.Repeat("*", 3), index, strings.Repeat("*", 3))
+			l = slices.Delete(l, index-1, index)
+		}
+	}
+	return l
 }
