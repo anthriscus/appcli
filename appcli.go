bdf1935b1294f4d8db797cd56df366cb23cc0dc6
Piers Kenyon <piers.kenyon@cgi.com>
2025-10-13 10:46:51 +0100

    chore: monday morning work in progress

--- appcli.go
+++ appcli.go
@@ -13,12 +13,12 @@ import (
 )
 
 const (
-	dataStorageFolder  string      = "appcli"
-	dataFileName       string      = "todolist.json"
-	openmode           int         = os.O_RDWR | os.O_CREATE
-	opentruncatemode   int         = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
-	readwrite          os.FileMode = 0600
-	friendlyDateFormat string      = time.RFC3339
+	dataStorageFolderName string      = "appcli"
+	dataFileName          string      = "todolist.json"
+	openFlag              int         = os.O_RDWR | os.O_CREATE
+	openTruncateFlag      int         = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	readwriteFileMode     os.FileMode = 0600
+	friendlyDateFormat    string      = time.RFC3339
 )
 
 // holds our item
@@ -49,61 +49,57 @@ var statusName = map[int]string{
 
 func main() {
 	// input flags
-	var flagAdd = flag.String("add", "", "add todolist item")
-	var flagUpdate = flag.String("update", "", "update task item id description")
-	var flagNotStart = flag.Bool("notstart", false, "not start a task item id number (not started)")
-	var flagStart = flag.Bool("start", false, "start a task item id number (starte)")
-	var flagComplete = flag.Bool("complete", false, "complete a task item id number (completed)")
-	var flagDelete = flag.Int("delete", -1, "delete a task item id number")
-	var flagList = flag.Bool("list", false, "list items in the todolist item")
+	var flagAdd = flag.String("add", "", "add todolist item (\"description\")")
+	var flagUpdate = flag.String("update", "", "update task item description (\"description\" id)")
+	var flagNotStart = flag.Bool("notstart", false, "set task item id number to not started ( id )")
+	var flagStart = flag.Bool("start", false, "start a task item ( id )")
+	var flagComplete = flag.Bool("complete", false, "complete a task item id ( id )")
+	var flagDelete = flag.Int("delete", -1, "delete a task item id number ( id )")
+	var flagList = flag.Bool("list", false, "list items in the todolist item ( id )")
 
-	// resolver the data folder
-	storageDir, storageErr := storagePath()
-	if storageErr != nil {
-		fmt.Printf("Storage path check failed:  %s\n", storageErr)
+	// resolve the appdata data sub folder
+	dir := createAppDataFolder(dataStorageFolderName)
+	if len(dir) == 0 {
+		fmt.Printf("Error:%s", "Cannot establish working data folder")
 		return
 	}
-	storageFile := fmt.Sprintf("%s\\%s", storageDir, dataFileName)
 
 	// init / pickup current list before process command
+	storageFile := fmt.Sprintf("%s\\%s", dir, dataFileName)
 	todoList := restore(storageFile)
 
 	// // grab the flag input state from command line
 	flag.Parse()
 
 	// process the flags
-	if *flagAdd != "" {
+	switch {
+	case *flagAdd != "":
 		itemKeys := collectKeys(todoList)
 		nextKey := highestKey(itemKeys) + 1
 		item := newTodoListItem(*flagAdd, StateNotStarted, nextKey)
 		todoList[nextKey] = *item
-	}
-	if *flagUpdate != "" {
+		listTask(todoList, nextKey)
+	case *flagUpdate != "":
 		index := argumentsFlagIndex(true, os.Args)
 		descriptionChange(todoList, index, *flagUpdate)
-	}
-	if *flagNotStart {
+		listTask(todoList, index)
+	case *flagNotStart:
 		index := argumentsFlagIndex(*flagNotStart, os.Args)
 		stateChange(todoList, index, StateNotStarted)
 		listTask(todoList, index)
-	}
-	if *flagStart {
+	case *flagStart:
 		index := argumentsFlagIndex(*flagStart, os.Args)
 		stateChange(todoList, index, StateStarted)
 		listTask(todoList, index)
-	}
-	if *flagComplete {
+	case *flagComplete:
 		index := argumentsFlagIndex(*flagComplete, os.Args)
 		stateChange(todoList, index, StateCompleted)
 		listTask(todoList, index)
-	}
-	if *flagDelete != -1 {
+	case *flagDelete != -1:
 		fmt.Printf("FlagDelete [%d]\n", *flagDelete)
 		deleteTask(todoList, *flagDelete)
 		listTask(todoList, -1)
-	}
-	if *flagList {
-		fmt.Printf("FlagList [%t]\n", *flagList)
+	case *flagList:
 		index := argumentsFlagIndex(*flagList, os.Args)
 		listTask(todoList, index)
 	}
@@ -124,26 +120,17 @@ func argumentsFlagIndex(flag bool, args []string) int {
 	return 0
 }
 
-// for sub folder
-func appName() string {
-	// return strings.Split(filepath.Base(os.Args[0]), ".")[0] // found problem with debugger renaming exe name!
-	return dataStorageFolder
-}
-
-func storagePath() (string, error) {
-	storageDir, storageErr := os.UserCacheDir()
-	storageDir = createAppPath(fmt.Sprintf("%s\\%s", storageDir, appName()))
-	return storageDir, storageErr
-}
-
-func createAppPath(fileFolder string) string {
-	os.Mkdir(fileFolder, readwrite)
-	return fileFolder
-}
-
-func createDataFile(fileName string, mode int) (*os.File, error) {
-	fi, err := os.OpenFile(fileName, mode, readwrite)
-	return fi, err
+func createAppDataFolder(applicationName string) string {
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		return ""
+	}
+	dir = dir + "\\" + applicationName
+	err = os.MkdirAll(dir, readwriteFileMode)
+	if err != nil {
+		return ""
+	}
+	return dir
 }
 
 // todolist item constructor
@@ -167,7 +154,7 @@ func stringifyList(list TodoListItems) string {
 
 // save list back to json
 func save(storageFile string, data []byte) {
-	destination, err := createDataFile(storageFile, opentruncatemode)
+	destination, err := os.OpenFile(storageFile, openTruncateFlag, readwriteFileMode)
 	if destination != nil {
 		defer destination.Close()
 	}
@@ -183,12 +170,12 @@ func saveData(w io.Writer, data []byte) int {
 
 // restore from json
 func restore(storageFile string) TodoListItems {
-	destination, err := createDataFile(storageFile, openmode)
-	if destination != nil {
-		defer destination.Close()
-	}
+	destination, err := os.OpenFile(storageFile, openFlag, readwriteFileMode)
 	if err != nil {
 		return TodoListItems{}
+	}
+	if destination != nil {
+		defer destination.Close()
 	}
 	return restoreList(destination)
 }
@@ -223,7 +210,7 @@ func generateId() string {
 // date string for task item
 func generatedFriendlyDate() string {
 	t := time.Now().UTC()
-	return fmt.Sprintf("%s", t.Format(friendlyDateFormat))
+	return "%s" + t.Format(friendlyDateFormat)
 }
 
 // task list report
@@ -267,8 +254,8 @@ func deleteTask(list TodoListItems, index int) {
 func stateChange(list TodoListItems, index int, state int) {
 	if len(list) > 0 {
 		if record, ok := list[index]; ok {
-			fmt.Printf("Changing task %d state to : %s\n", index, statusName[state])
 			fmt.Printf("Current state: %s", statusName[list[index].State])
+			fmt.Printf("Changing task %d state to : %s\n", index, statusName[state])
 			record.State = state
 			list[index] = record
 		}
@@ -278,8 +265,8 @@ func stateChange(list TodoListItems, index int, state int) {
 func descriptionChange(list TodoListItems, index int, newDescription string) {
 	if len(list) > 0 {
 		if record, ok := list[index]; ok {
-			fmt.Printf("Changing task %d description to : %s\n", index, newDescription)
 			fmt.Printf("Current description: %s", list[index].Description)
+			fmt.Printf("Changing task %d description to : %s\n", index, newDescription)
 			record.Description = newDescription
 			list[index] = record
 		}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anthriscus/appcli
+
+go 1.25.2
